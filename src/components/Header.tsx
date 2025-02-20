f0c931e75cf6fcfb3d5fa4466c6bc0d2e376d720
Agustin <latierramedia@gmail.com>
2025-02-20 17:04:46 +0100

    arreglo ruta de imagenes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,14 +19,14 @@ export default function Header({ cart, dispatch } : HeaderProps ) {
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div 
                             className="carrito"
                         >
-                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
                                 {isEmpty ? (
@@ -49,7 +49,7 @@ export default function Header({ cart, dispatch } : HeaderProps ) {
                                                     <td>
                                                         <img 
                                                             className="img-fluid" 
-                                                            src={`/img/${guitar.image}.jpg`}
+                                                            src={`./img/${guitar.image}.jpg`}
                                                             alt="imagen guitarra" 
                                                         />
                                                     </td>
